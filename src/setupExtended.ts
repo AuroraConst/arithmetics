e34c6e3f5ac835628096c08e6137fd301df7913c
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2025-01-22 14:21:08 -0500

    First commit
    Langium arithmetics example

---
+++ src/setupExtended.ts
@@ -0,0 +1,58 @@
+import { MonacoEditorLanguageClientWrapper, UserConfig } from 'monaco-editor-wrapper';
+import { configureWorker, defineUserServices } from './setupCommon.js';
+
+export const setupConfigExtended = (): UserConfig => {
+    const extensionFilesOrContents = new Map();
+    extensionFilesOrContents.set('/language-configuration.json', new URL('../language-configuration.json', import.meta.url));
+    extensionFilesOrContents.set('/arithmetics-grammar.json', new URL('../syntaxes/arithmetics.tmLanguage.json', import.meta.url));
+
+    return {
+        wrapperConfig: {
+            serviceConfig: defineUserServices(),
+            editorAppConfig: {
+                $type: 'extended',
+                languageId: 'arithmetics',
+                code: `// Arithmetics is running in the web!`,
+                useDiffEditor: false,
+                extensions: [{
+                    config: {
+                        name: 'arithmetics-web',
+                        publisher: 'generator-langium',
+                        version: '1.0.0',
+                        engines: {
+                            vscode: '*'
+                        },
+                        contributes: {
+                            languages: [{
+                                id: 'arithmetics',
+                                extensions: [
+                                    '.arithmetics'
+                                ],
+                                configuration: './language-configuration.json'
+                            }],
+                            grammars: [{
+                                language: 'arithmetics',
+                                scopeName: 'source.arithmetics',
+                                path: './arithmetics-grammar.json'
+                            }]
+                        }
+                    },
+                    filesOrContents: extensionFilesOrContents,
+                }],                
+                userConfiguration: {
+                    json: JSON.stringify({
+                        'workbench.colorTheme': 'Default Dark Modern',
+                        'editor.semanticHighlighting.enabled': true
+                    })
+                }
+            }
+        },
+        languageClientConfig: configureWorker()
+    };
+};
+
+export const executeExtended = async (htmlElement: HTMLElement) => {
+    const userConfig = setupConfigExtended();
+    const wrapper = new MonacoEditorLanguageClientWrapper();
+    await wrapper.initAndStart(userConfig, htmlElement);
+};
